8ee5b4ad7dcd8577cf265911468a7b8351d4223b
Chibby332 <dead777boy@gmail.com>
2023-03-04 15:40:18 +0700

    2.2.1.1 - 2.2.3.1

--- LAB2/LAB2.cpp
+++ LAB2/LAB2.cpp
@@ -1,20 +1,129 @@
-﻿// LAB2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <exception>
+#include <string.h>
+using namespace std;
 
-#include <iostream>
+//2.2.1.1 and 2.2.1.2
+
+void Sort(double* values, int count);
+void DemoSort();
+
+//2.2.3.1
+
+void DemoRectangle();
+void DemoFlight();
+void DemoMovie();
+void DemoTime();
+
+//2.2.2.1
+
+struct Rectangle {
+    double Width;
+    double Height;
+    string Color;
+};
+struct Flight {
+    string Departure;
+    string Destination;
+    int Time;
+};
+struct Movie {
+    string Name;
+    int Length;
+    int Year;
+    string Genre;
+    double Rating;
+};
+struct Time {
+    int Hour;
+    int Min;
+    int Sec;
+};
+
+//2.2.2.2
+
+struct Contact {
+    string Name;
+    string Number;
+    int Age;
+};
+struct Song {
+    int Length;
+    string Name;
+    string Genre;
+};
+struct Discipline {
+    string Name;
+    int Grade;
+    int Duration;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+//2.2.1.1 and 2.2.1.2
+void Sort(double* values, int count) {
+    double swap;
+    if (count >= 0) {
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < count; j++)
+            {
+                if (values[i] < values[j])
+                {
+                    swap = values[i];
+                    values[i] = values[j];
+                    values[j] = swap;
+                }
+            }
+        }
+    }
+    else {
+        throw exception();
+    }
+}
+void DemoSort()
+{
+    int count = 5;
+    double* values = new double[count] {100.0, 249.0, 12.0, 45.0, 23.5};
+    try {
+        Sort(values, count);
+    }
+    catch (exception) {
+        cout << "Exception catched!";
+    }
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+//2.2.3.1
+
+void DemoRectangle() {
+    Rectangle rectangle;
+    rectangle.Color = "Red";
+    rectangle.Height = 3.5;
+    rectangle.Width = 7.2;
+}
+void DemoFlight() {
+    Flight flight;
+    flight.Departure = "Moscow";
+    flight.Destination = "Berlin";
+    flight.Time = 360;
+}
+void DemoMovie() {
+    Movie movie;
+    movie.Genre = "Drama";
+    movie.Length = 180;
+    movie.Name = "Star Wars";
+    movie.Rating = 7.2;
+    movie.Year = 2323;
+
+}
+void DemoTime() {
+    Time time;
+    time.Hour = 12;
+    time.Min = 45;
+    time.Sec = 19;
+}
